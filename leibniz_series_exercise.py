1662b21ef70fd2e85e62b7a7e557999815292686
nareenz125-creator <nareenz125@gmail.com>
2025-11-10 17:39:40 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,6 @@
 def approximate_pi (n_terms):
-  pi_estimate=0
-  for n in range (n_terms):
-    pi_estimate+=((-1)**2/(2*n+1))
-  return 4*total
-    
+  pi_estimate = 0
+  for n in range(n_terms):
+    pi_estimate += ((-1)**) / (2*n + 1)
+  pi_estimate *= 4
+  return pi_estimate
